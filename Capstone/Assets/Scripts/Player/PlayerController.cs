aa1e44df11559f8fb3cadab273487142b3ccac1f
Bill Pham <47372471+billphaml@users.noreply.github.com>
2021-04-05 16:20:03 -0700

    Camera updates
    Changed player cameras to render only if the camera belongs to the client.
    
    Added camera for menu and disable on join.

--- Capstone/Assets/Scripts/Player/PlayerController.cs
+++ Capstone/Assets/Scripts/Player/PlayerController.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAPI;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    [SerializeField]
-    private SpriteRenderer playerColor;
+    [SerializeField] private SpriteRenderer playerColor;
+
+    [SerializeField] private Transform cameraTransform;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (!IsLocalPlayer)
+        {
+            cameraTransform.GetComponent<AudioListener>().enabled = false;
+            cameraTransform.GetComponent<Camera>().enabled = false;
+        }
+
         playerColor.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
 }
