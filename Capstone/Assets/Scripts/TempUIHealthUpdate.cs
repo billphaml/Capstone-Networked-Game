c140d5355054dbabe2fc8d088706e5772ea430b4
Bill Pham <47372471+billphaml@users.noreply.github.com>
2021-05-03 23:06:46 -0700

    Added health

--- Capstone/Assets/Scripts/TempUIHealthUpdate.cs
+++ Capstone/Assets/Scripts/TempUIHealthUpdate.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using MLAPI;
+using TMPro;
 
-public class TempUIHealthUpdate : MonoBehaviour
+public class TempUIHealthUpdate : NetworkBehaviour
 {
     private PlayerHealth player = null;
+
+    public TextMeshProUGUI ui = null;
 
     // Update is called once per frame
     void Update()
     {
         if (player == null)
         {
-            //player = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.gameObject;
+            player = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.gameObject.GetComponent<PlayerHealth>();
         }
+
+        ui.text = "Health: " + player.Health.Value + "/100";
     }
 }
