1cbd14125f699593836ad899a9823b7c2110fe4b
MadMehx <65829543+MadMehx@users.noreply.github.com>
2021-05-09 13:12:38 -0700

    recipe and crafting item skeleton

---
+++ Capstone/Assets/Resources/ScriptableObjects/Items/CraftingItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingItem : MonoBehaviour
+{
+    //will return name and item type of object for the purposes of crafting.
+    public enum ItemType { 
+        //insert defined types here
+    }
+}
