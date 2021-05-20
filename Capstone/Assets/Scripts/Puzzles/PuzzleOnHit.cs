f2c1b13d552fd2b820708d262ea164e206ef1908
Bill Pham <47372471+billphaml@users.noreply.github.com>
2021-05-19 23:25:47 -0700

    Added animations to puzzle triggers

--- Capstone/Assets/Scripts/Puzzles/PuzzleOnHit.cs
+++ Capstone/Assets/Scripts/Puzzles/PuzzleOnHit.cs
@@ -13,23 +13,52 @@ public class PuzzleOnHit : PuzzleTrigger
 {
     public bool isHit = false;
 
-    [SerializeField] private float hitDuration = 15f;
+    [SerializeField] private float hitDuration;
 
-    [ServerRpc]
+    [SerializeField] private SpriteRenderer runes;
+
+    [SerializeField] private float lerpSpeed;
+
+    private Color curColor;
+
+    private Color targetColor;
+
+    [ServerRpc(RequireOwnership = false)]
     public void HitServerRpc()
     {
         if (!isHit)
         {
             isHit = true;
+            EnableRunesClientRpc();
             TriggerServerRpc();
-            Unhit();
+            StartCoroutine(Unhit());
         }
+    }
+
+    [ClientRpc]
+    private void EnableRunesClientRpc()
+    {
+        targetColor = new Color(1, 1, 1, 1);
     }
 
     IEnumerator Unhit()
     {
         yield return new WaitForSeconds(hitDuration);
         isHit = false;
+        DisableRunesClientRpc();
         UntriggerServerRpc();
+    }
+
+    [ClientRpc]
+    private void DisableRunesClientRpc()
+    {
+        targetColor = new Color(1, 1, 1, 0);
+    }
+
+    private void Update()
+    {
+        curColor = Color.Lerp(curColor, targetColor, lerpSpeed * Time.deltaTime);
+
+        runes.color = curColor;
     }
 }
