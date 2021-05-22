ae542400ec6f1b3edad434ddb9cc24d4677239cc
Bill Pham <47372471+billphaml@users.noreply.github.com>
2021-05-21 21:28:55 -0700

    Added chest to puzzle

---
+++ Capstone/Assets/Scripts/Interact/ChestInteractable.cs
@@ -0,0 +1,43 @@
+/******************************************************************************
+ * This Class is derived from interactable, and is used to create a chest
+ * object that can be opened to give an item. The chest is removed after being
+ * interacted with.
+ * 
+ * Authors: Bill, Hamza, Max, Ryan
+ * ***************************************************************************/
+
+using UnityEngine;
+using MLAPI;
+using MLAPI.Messaging;
+
+public class ChestInteractable : Interactable
+{
+    [SerializeField] private ItemBehavior[] items;
+
+    protected override void Interact()
+    {
+        Debug.Log("Interacted with " + transform.name);
+
+        var num = Random.Range(0, items.Length - 1);
+        var item = items[num];
+        NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.GetComponent<PlayerStat>().AddItemPrefab(item);
+    }
+
+    protected override void Update()
+    {
+        if (isInteractDisplayed)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Interact();
+                DestroyServerRpc();
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void DestroyServerRpc()
+    {
+        Destroy(gameObject);
+    }
+}
